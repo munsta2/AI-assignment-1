6c73215b5c5bbe70b82a02484138aeb335244d73
Tidomann <tidomann@gmail.com>
2022-10-07 23:21:50 -0400

    code cleanup

--- question4.py
+++ question4.py
@@ -5,12 +5,11 @@ from states import State
 
 max_child_Size = 0
 
+
 def main():
     start = timeit.default_timer()
     tile_objects = []
     paired_values, board = prepare_problem_from_input_text('input.txt', tile_objects)
-    # board_size = len(board) * len(board[0])
-    # unplaced_tiles = [True] * board_size
     # Creating a list of booleans equal in length to the number of tiles to reference to check
     # if a tile has been placed
     unplaced_tiles = [True] * len(tile_objects)
@@ -48,7 +47,7 @@ def A_star(row, col, board, tiles, paired_values, unplaced_tiles):
 
 
 def a_star_helper(in_state, tile_objects, paired_values, fringe_children):
-    # Open the root node and add it's children to the fringe_children
+    # Open the root node and add its children to the fringe_children
     in_state.find_valid_children(tile_objects, paired_values)
     fringe_children = fringe_children + in_state.children
     # If the root note does not have any valid children, the problem cannot be solved at this position
@@ -56,7 +55,7 @@ def a_star_helper(in_state, tile_objects, paired_values, fringe_children):
         return [sys.maxsize, None]
 
     goal_states = []
-    while(True):
+    while True:
         # If no states exist in the fringe_children, the problem cannot be solved with these positions
         if not fringe_children:
             return [sys.maxsize, None]
@@ -71,13 +70,15 @@ def a_star_helper(in_state, tile_objects, paired_values, fringe_children):
 
         # If we have found goal states, check to see if these are better than any possible fringe state
         if goal_states:
-            # Find out best goal state
+
+            # Find the best goal state
             minimum_goal_cost = goal_states[0].cost
             best_goal = goal_states[0]
             for goal in goal_states:
                 if minimum_goal_cost > goal.cost:
                     minimum_goal_cost = goal.cost
                     best_goal = goal
+
             # If the best goal state is better than any fringe state, A* has completed
             if minimum_goal_cost < minimum_fn:
                 print("Board State:")
@@ -92,18 +93,23 @@ def a_star_helper(in_state, tile_objects, paired_values, fringe_children):
         # Remove the best state from the fringe state. Expand the state by adding it's children to the fringe
         fringe_children.remove(chosen_state)
         chosen_state.find_valid_children(tile_objects, paired_values)
+
         # Test the new states to see if the board state is identical in tile placement to existing fringe states
         identity_list = []
         for fringe in fringe_children:
             identity_list.append(identify_board(fringe.board))
+
+        # Add the children to the fringe states
         for child in chosen_state.children:
+            # If the new child has a unique board configuration, add it
             if not identify_board(child.board) in identity_list:
                 fringe_children.append(child)
+            # The child has a configuration the same as some of our fringe states, only keep the most efficient path
             else:
                 # There exists a duplicate board state, we need to keep only the most efficient version
                 duplicate_state_object_list_for_comparison = []  # aka bad boi
                 # While a duplicate state exists, remove them and
-                while (identify_board(child.board) in identity_list):
+                while identify_board(child.board) in identity_list:
                     # Find the index of the duplicate state
                     index_of_duplicate = identity_list.index(identify_board(child.board))
 
@@ -121,15 +127,14 @@ def a_star_helper(in_state, tile_objects, paired_values, fringe_children):
                     if state.cost < min_cost:
                         min_state = state
                         min_cost = state.cost
-                # only re-add the most effecient state to our fringe children
+                # only re-add the most efficient state to our fringe children
                 fringe_children.append(min_state)
 
+        # Track the maximum fringe size for debugging
         global max_child_Size
         if max_child_Size < len(fringe_children):
             max_child_Size = len(fringe_children)
-        # repeat while loop
-
-
+        # end of while loop
 
 
 def prepare_problem_from_input_text(file, tiles):
